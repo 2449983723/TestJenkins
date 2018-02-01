cabeca15b0b60d312d36c89a3085e2061d738380
liunian <liun759@13322.com>
2018-02-01 10:22:08 +0800

    修改配置

--- jenkinsQR.py
+++ jenkinsQR.py
@@ -4,7 +4,7 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 import qrcode
 import os
-
+import traceback
 
 log_file_name = 'log_python.txt'
 # 清空文件内容
@@ -20,27 +20,31 @@ def dumpToFile(content):
     f.close()
 
 
-# 编译类型（debug还是release）
-build_type = sys.argv[1]
-# 获得编译时间
-build_time = sys.argv[2]
-# 二维码存储的地址
-or_code_path = sys.argv[3] + '/' + build_type + '/' + build_time
-# apk名称
-apkName = sys.argv[4]
+try:
+    # 编译类型（debug还是release）
+    build_type = sys.argv[1]
+    # 获得编译时间
+    build_time = sys.argv[2]
+    # 二维码存储的地址
+    or_code_path = sys.argv[3] + '/' + build_type + '/' + build_time
+    # apk名称
+    apkName = sys.argv[4]
 
-apk_path = 'http://10.41.12.20:8080/jenkinsApk/testJenkins'+ '/' + build_type + '/' + build_time + '/' + apkName
+    apk_path = 'http://10.41.12.20:8080/jenkinsApk/testJenkins'+ '/' + build_type + '/' + build_time + '/' + apkName
 
-if not os.path.exists(or_code_path):
-    os.makedirs(or_code_path)
+    if not os.path.exists(or_code_path):
+        os.makedirs(or_code_path)
 
-or_code_file_path = or_code_path + '/' + build_time + '.png'
-if os.path.exists(or_code_file_path):
-    os.remove(or_code_file_path)
-clearFile()
-dumpToFile(apk_path)
-dumpToFile(or_code_file_path)
+    or_code_file_path = or_code_path + '/' + build_time + '.png'
+    if os.path.exists(or_code_file_path):
+        os.remove(or_code_file_path)
+    clearFile()
+    dumpToFile(apk_path)
+    dumpToFile(or_code_file_path)
 
-# 生成二维码
-img = qrcode.make(apk_path)
-img.save(or_code_file_path)
+    # 生成二维码
+    img = qrcode.make(apk_path)
+    img.save(or_code_file_path)
+except Exception, e:
+    print 'traceback.print_exc():'
+    traceback.print_exc()
